850ed7795be6723c20619e26c4d99af2497e7d6d
Joshua Soriano <54717468+JoshuaDKT@users.noreply.github.com>
2025-02-02 20:17:37 -0500

    Delete Uber Customer Review Analysis directory

--- Uber Customer Review Analysis/main.py
+++
@@ -1,8 +0,0 @@
-from modules import data_preparation
-
-
-def main():
-    prepare_data.prepare_data()
-
-if '__name__' == '__main__':
-    main()
--- Uber Customer Review Analysis/test/scratch.py
+++
@@ -1,4 +0,0 @@
-for i, value in enumerate(polarity_review_count['negative_polarity']):
-    axes[1, 0].text(i - bar_width / 2, value + 0.5, str(value), ha='center', va='bottom')
-for i, value in enumerate(polarity_review_count['positive_polarity']):
-    axes[1, 0].text(i + bar_width / 2, value + 0.5, str(value), ha='center', va='bottom')
