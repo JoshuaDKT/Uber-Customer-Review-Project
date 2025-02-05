23cb45d82dac04ab5ef1aad81073a7b1f4691461
Joshua Soriano <54717468+JoshuaDKT@users.noreply.github.com>
2025-02-04 19:10:09 -0500

    Add files via upload
    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from modules import data_preparation
+from modules.data_preparation import data_preparation
+from modules.data_visualization import data_visualization
 
 
-def main():
-    prepare_data.prepare_data()
+def main(file_path):
+    df = data_preparation.prepare_data(file_path)
+    data_visualization.visualize_data(df)
 
-if '__name__' == '__main__':
-    main()
+
+if __name__ == '__main__':
+    main(r'D:\Projects\PyCharm\Uber Customer Review Analysis\data\uber_reviews_without_reviewid.csv')
